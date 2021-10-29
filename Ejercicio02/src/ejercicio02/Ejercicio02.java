660a2f4d3a55b13ac82af8d691dc4628f5b4eb9f
Jeremy <jfjaramillo19@utpl.edu.ec>
2021-10-29 12:11:32 -0500

    práctica y experimentación

---
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio02;
+
+/**
+ *
+ * @author fabri
+ */
+public class Ejercicio02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int variable1 = 10;
+        int variable3 = 2;
+        
+        System.out.println("------Solución a problema------");
+        //Math.pou me permite elevar un número
+        
+        double resultado = variable1 + Math.pow(variable1, variable3);
+        
+        System.out.println(resultado);
+        
+    }
+    
+}
